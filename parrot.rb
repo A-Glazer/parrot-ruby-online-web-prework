795a380be4e50ed0b37043adc5b4877708d13e38
A Glazer <avigayilglazer@gmail.com>
2018-12-24 11:06:29 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(phrase = "Squawk")
-parrot = "#{phrase}!"
+def parrot(phrase = "Squawk!")
+parrot = "#{phrase}"
 puts parrot
 return parrot
 end
